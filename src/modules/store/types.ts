811e84a13807bd7d271b4fe49de0dcddbc64b25b
ha <some@email.com>
2024-03-28 17:47:04 -0500

    very iinitial

--- src/modules/store/types.ts
+++ src/modules/store/types.ts
@@ -4,6 +4,9 @@ export interface Store {
   logo: string;
   banner: string;
   instagram: string;
+  facebook: string;
+  tiktok: string;
+  youtube: string;
   whatsapp: string;
   phone: string;
 }
